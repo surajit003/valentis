f19d3c7bf47ac728ac10f0608bb698216c965e45
surajit <surajit@poweredbypeople.io>
2020-08-07 01:43:58 +0300

    removed duplicate code

--- clinic/admin.py
+++ clinic/admin.py
@@ -28,37 +28,3 @@ class DiagnosisAdmin(admin.ModelAdmin):
     list_display = ['created', 'last_updated', 'code', 'name']
 
 admin.site.register(Diagnosis, DiagnosisAdmin)
-
-
-from django.contrib import admin
-from django import forms
-from .models import PatientVisit, Diagnosis
-
-class PatientVisitAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = PatientVisit
-        fields = '__all__'
-
-
-class PatientVisitAdmin(admin.ModelAdmin):
-    form = PatientVisitAdminForm
-    list_display = ['name', 'slug', 'created', 'last_updated', 'patient_no', 'radiology_no', 'notes', 'diagnosis', 'prescription_id', 'status', 'triage_id', 'examination', 'plan_of_managemnt', 'query_diagnosis', 'his_presenting_illness', 'attending_doctor','visit_id','diag_search']
-
-admin.site.register(PatientVisit, PatientVisitAdmin)
-
-
-class DiagnosisAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Diagnosis
-        fields = '__all__'
-
-
-class DiagnosisAdmin(admin.ModelAdmin):
-    form = DiagnosisAdminForm
-    list_display = ['created', 'last_updated', 'code', 'name']
-
-admin.site.register(Diagnosis, DiagnosisAdmin)
-
-
